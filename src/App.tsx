52a36f31fa6317d0e2d265693438723c36b3058f
syednayeemjaffer <nayeems199@gmail.com>
2026-04-06 23:47:41 +0530

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
+const routerBasename = "/black-panda-luxe";
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={routerBasename}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
