92981d99f6e1cb0ec11ca136327947d41965144c
syednayeemjaffer <nayeems199@gmail.com>
2026-04-06 22:55:17 +0530

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base:"/black-panda-luxe/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
